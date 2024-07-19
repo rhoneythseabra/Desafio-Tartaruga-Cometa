090d793dbc673f92c919ac7babba3517d4f81ffc
Rhoney Seabra <rhoneyholanda@gmail.com>
2024-07-18 22:26:20 -0300

    Adicionando as entidades Remetente e Destinatário

---
+++ src/main/java/com/desafio_entrega/Desafio/Tartaruga/Cometa/entregas/model/Remetente.java
@@ -0,0 +1,58 @@
+package com.desafio_entrega.Desafio.Tartaruga.Cometa.entregas.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Remetente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nomeOuRazao;
+    private String cpfOuCnpj;
+    private String endereco;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeOuRazao() {
+        return nomeOuRazao;
+    }
+
+    public void setNomeOuRazao(String nomeOuRazao) {
+        this.nomeOuRazao = nomeOuRazao;
+    }
+
+    public String getCpfOuCnpj() {
+        return cpfOuCnpj;
+    }
+
+    public void setCpfOuCnpj(String cpfOuCnpj) {
+        this.cpfOuCnpj = cpfOuCnpj;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
